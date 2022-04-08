087d1ca7ff4a2d43f313c2ace1cd37eb7a38c46b
zafs23 <sajiazafreen@u.boisestate.edu>
2022-04-08 15:08:40 -0700

    refactor views classes

--- app/painting/views.py
+++ app/painting/views.py
@@ -7,39 +7,38 @@ from core.models import Category, Supply
 from painting import serializers
 
 
-class CategoryViewSet(viewsets.GenericViewSet,
-                      mixins.ListModelMixin,
-                      mixins.CreateModelMixin):
-    """Manage category in the database"""
+# as the category and supply viewset classes have so much in common
+# it will be better to refactor the common fuctionality in a single
+# class
+class BasePaintingAttrViewSet(viewsets.GenericViewSet,
+                              mixins.ListModelMixin,
+                              mixins.CreateModelMixin):
+    """Common viewset for user owned painting attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new object (category/supply)"""
+        serializer.save(user=self.request.user)
+
+
+class CategoryViewSet(BasePaintingAttrViewSet):
+    """Manage category in the database"""
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
     queryset = Category.objects.all()
     serializer_class = serializers.CategorySerializer
 
-    def get_queryset(self):  # any filtering can be applied here
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        """Create a new category"""
-        serializer.save(user=self.request.user)
-
-
-class SupplyViewSet(viewsets.GenericViewSet,
-                    mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
+class SupplyViewSet(BasePaintingAttrViewSet):
     """Manage supply in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
     queryset = Supply.objects.all()
     serializer_class = serializers.SupplySerializer
-
-    def get_queryset(self):  # any filtering can be applied here
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new SUPPLY"""
-        serializer.save(user=self.request.user)
 # Create your views here.
 # going to use list model fuction from the rest rest_framework
